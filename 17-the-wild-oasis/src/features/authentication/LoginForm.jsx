355a8a6d2e8c0cbcc9178145639b51c385713aae
Anand Kumar Mishra <110248822+akm-xdd@users.noreply.github.com>
2023-08-15 12:54:19 +0530

    User Authentication Added

--- 17-the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ 17-the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -3,12 +3,23 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useLogin } from "./useLogin";
+import SpinnerMini from "../../ui/SpinnerMini";
+
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("anand@example.com");
+  const [password, setPassword] = useState("password");
 
-  function handleSubmit() {}
+  const { login, isLoading } = useLogin()
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return
+    login({ email, password })
+
+
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -20,6 +31,7 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRow>
       <FormRow label="Password" orientation="vertical">
@@ -29,10 +41,13 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRow>
       <FormRow orientation="vertical">
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoading}>
+          {!isLoading ? "Login" : <SpinnerMini />}
+        </Button>
       </FormRow>
     </Form>
   );
--- 17-the-wild-oasis/src/ui/FormRow.jsx
+++ 17-the-wild-oasis/src/ui/FormRow.jsx
@@ -1,30 +1,10 @@
 import styled from "styled-components";
 
 const StyledFormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
   padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
 `;
 
 const Label = styled.label`
@@ -36,7 +16,7 @@ const Error = styled.span`
   color: var(--color-red-700);
 `;
 
-function FormRow({ label, error, children }) {
+function FormRowVertical({ label, error, children }) {
   return (
     <StyledFormRow>
       {label && <Label htmlFor={children.props.id}>{label}</Label>}
@@ -46,4 +26,4 @@ function FormRow({ label, error, children }) {
   );
 }
 
-export default FormRow;
+export default FormRowVertical;
