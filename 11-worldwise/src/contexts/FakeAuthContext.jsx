bd21af645c5fba30b80aeaad3d91405bc5a5f179
Anand Kumar Mishra <110248822+akm-xdd@users.noreply.github.com>
2023-08-15 20:15:47 +0530

    updated

--- 11-worldwise/src/contexts/FakeAuthContext.jsx
+++ 11-worldwise/src/contexts/FakeAuthContext.jsx
@@ -19,10 +19,10 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
+  name: "Anand",
+  email: "anand@example.com",
   password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
+  avatar: "https://i.pravatar.cc/100?u=zz", //Random Avatar
 };
 
 function AuthProvider({ children }) {
